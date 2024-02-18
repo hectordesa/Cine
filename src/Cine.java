8f588cc3b91813d3ef1713c1ec4d364618b53584
hectordesa <hectordelsan@gmail.com>
2024-02-18 20:34:08 +0100

    feat: Cine funciones añadidas

--- src/Cine.java
+++ src/Cine.java
@@ -42,4 +42,35 @@ public class Cine {
         this.salas = salas;
     }
 
+    public void mostrarsalas() {
+        for (int i = 0; i<salas.length;i++){
+            getSalas()[i].setPelicula(peliculas[i]);
+            System.out.println("SALA " + (i+1));
+            System.out.println("");
+            System.out.println("PELICULA: " + getSalas()[i].getPelicula().getNombre());
+            System.out.println("DURACION: " + getSalas()[i].getPelicula().getDuracion()+ " minutos");
+            System.out.println("--------------------------------------------");
+        }
+    }
+
+    public void compra(int sala, int fila, int col, String correo){
+        getSalas()[sala].getButacas()[fila-1][col-1] = correo;
+    }
+
+    public void mostrarmatriz(int sala, String correo){
+        if (sala < salas.length && sala > 0){
+            for(int i = 0; i<getSalas()[sala].getButacas().length; i++){
+                for (int j = 0; j<getSalas()[sala].getButacas()[i].length;j++){
+                    if(getSalas()[sala].getButacas()[i][j] == null) {
+                        System.out.print("libre" + " ");
+                    }else {
+                        System.out.print(correo + "  ");
+                    }
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("LA SALA SELECCIONADA NO EXISTE");
+        }
+    }
 }
