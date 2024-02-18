f1e5e0176fe3e3f7e55ea979bd72bc402203c291
hectordesa <hectordelsan@gmail.com>
2024-02-18 19:53:45 +0100

    feat: Pelicula getset

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,2 +1,25 @@
 public class Pelicula {
+    private String nombre;
+    private int duracion;
+
+    public Pelicula(String nombre, int duracion) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
 }
